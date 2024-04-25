48c4dc428931d1a843b707514d3ff630f0e87858
nicolasb <nicolasbotellomendoza@gmail.com>
2024-04-24 23:08:18 -0500

    Metodo convertir que devuelve el json de la conversion

--- src/com/conversorMonedas/modelos/ConvertidorApi.java
+++ src/com/conversorMonedas/modelos/ConvertidorApi.java
@@ -1,5 +1,24 @@
 package com.conversorMonedas.modelos;
 
-public class ConvertidorApi {
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
 
+public class ConvertidorApi {
+    public String apikey = "453a870bba07c3c1114b6a14";
+    public void convertir(String base, String destino, int cantidad){
+        HttpClient client = HttpClient.newHttpClient();
+        URI direccion = URI.create("https://v6.exchangerate-api.com/v6/"
+                +this.apikey+"/pair/"+base+"/"+destino+"/"+cantidad);
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(direccion)
+                .build();
+        try {
+            HttpResponse<String> response = client.send( request, HttpResponse.BodyHandlers.ofString());
+            System.out.println(response.body());
+        }catch (Exception e){
+            System.out.println("Error: " + e.getMessage());
+        }
+    }
 }
---
+++ src/com/conversorMonedas/principal/Principal.java
@@ -0,0 +1,2 @@
+package com.conversorMonedas.principal;public class Principal {
+}
